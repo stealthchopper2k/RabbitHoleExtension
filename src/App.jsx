591f98d24d62ea3445f9609a490e608106cc2516
stealthchopper2k <stealthchopper2k@gmail.com>
2023-10-02 15:00:51 +0100

    Call browser client through extension proxy to log user in and use cookie to extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from "react";
-import { signIn } from "./login.js";
+import axios, {isCancel, AxiosError} from 'axios';
 import "./styles.css";
 
 export default function App() {
-  const [userSignedIn, setUserSignedIn] = useState(false)
+  const [user, setUser] = useState({})
+
+  const handleLogin = () => {
+    chrome.tabs.create({url: 'http://localhost:3000/auth/google', selected:true, active:true})
+  }
+
+  const handleLogout = () => {
+    axios.get("http://localhost:3000/auth/logout").then(response => console.log(response))
+  }
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/auth/me").then((res) => {
+      console.log(res.data)
+      setUser(res.data)
+    })
+  },[])
   
   return (
       <div className="flex p-10">
       <h1 className="text-3xl">RabbitHole</h1>
-      <div className="bg-red p-8">
-        <a href="/auth/google">Login With Google</a>
+      <div className="flex justify-center items-center">
+        {user ? <button className="py-4 px-3 bg-green-500 text-white m-2" onClick={handleLogout}>
+          Logout
+        </button> :
+          <button className="py-4 px-3 bg-green-500 text-white m-2" onClick={handleLogin}>Login with Google</button>}
         </div>
       </div>
   );
 }
+
+
